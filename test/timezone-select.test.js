43fa3a03fd672f406e3d1cb0c935fb077de86125
Troy Whiteley <troy@somanyscientists.com>
2013-07-31 15:32:57 -0700

    Added moment-timezone support.

--- test/timezone-select.test.js
+++ test/timezone-select.test.js
@@ -12,16 +12,16 @@ suite('timezone-select', function() {
     test('Timezone should return array of jQuery object to allow chaining',function(){
       assert.deepEqual(select.timezone(), select);
     });
-    test('jQuery element should have 13 options by default',function(){
-      assert.equal(select.find('option').length, 13);
+    test('jQuery element should have options',function(){
+      assert.equal(select.find('option').length > 1, true);
     });
     test('jQuery element should have current timezone selected or first one if no available',function(){
-      var currentTimezoneKey = ((new Date()).getTimezoneOffset() / - 60).toFixed(1);
+      var currentTimezoneKey = moment().format('Z');
       var isValueInside = (function(){
         var is = false;
 
         select.find('option').each(function(){
-          if ($(this).val() == currentTimezoneKey){
+          if ($(this).data('offset') == currentTimezoneKey){
             is = true;
             return;
           }
@@ -30,18 +30,11 @@ suite('timezone-select', function() {
         return is;
       })();
       if (isValueInside){
-        assert.equal(select.val(),currentTimezoneKey);
+        assert.equal(select.find('option[value="' + select.val() + '"]').data('offset'), currentTimezoneKey);
       }
       else {
-        assert.equal(select.val(),select.find('option').first().val());
+        assert.equal(select.val(), select.find('option').first().val());
       }
-    });
-    test('Given a number of cities, should same number of options',function(){
-      var mock = [{value: "-10.0", name: "Honolulu - HST UTC-10"},
-        {value: "-7.0", name: "San Francisco - PDT UTC-7"}];
-
-      select.timezone(mock);
-      assert.equal(mock.length,select.find('option').length);
     });
   });
 });
